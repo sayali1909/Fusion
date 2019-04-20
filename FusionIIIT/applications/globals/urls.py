acb4f43e97f73c0f757511a3e9fcc4e1dff2d0ab
sayali <sayali.cool09@gmail.com>
2019-04-20 11:18:15 +0530

    Manually resolved merge conflicts

--- FusionIIIT/applications/globals/urls.py
+++ FusionIIIT/applications/globals/urls.py
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-#from django.conf.urls import url
-
-#from . import views
-
-#app_name = 'globals'
-
-#urlpatterns = [
-
- #  url(r'^dashboard/$', views.dashboard, name='dashboard'),
-  #  url(r'^about/', views.about, name='about'),
-    # Feedback and issues url
-   # url(r'^feedback/$', views.feedback, name="feedback"),
-    #url(r'^issue/$', views.issue, name="issue"),
-   # url(r'^view_issue/(?P<id>\d+)/$', views.view_issue, name="view_issue"),
-    #url(r'^support_issue/(?P<id>\d+)/$', views.support_issue, name="support_issue"),
-    #url(r'^logout/$', views.logout_view, name="logout_view"),
-#]
-
-
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'globals'
-
-urlpatterns = [
-
-    url(r'^$', views.index, name='index'),
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
-    url(r'^about/', views.about, name='about'),
-    url(r'^profile/(?P<username>/w+)/$', views.profile, name='profile'),
-    url(r'^profile/$', views.profile, name='profile'),
-    url(r'^search/$', views.search, name='search'),
-    # Feedback and issues url
-    url(r'^feedback/$', views.feedback, name="feedback"),
-    url(r'^issue/$', views.issue, name="issue"),
-    url(r'^view_issue/(?P<id>\d+)/$', views.view_issue, name="view_issue"),
-    url(r'^support_issue/(?P<id>\d+)/$', views.support_issue, name="support_issue"),
-    url(r'^logout/$', views.logout_view, name="logout_view"),
-]
-=======
 from django.conf.urls import url
 
 from . import views
@@ -63,4 +21,3 @@ urlpatterns = [
     url(r'^support_issue/(?P<id>\d+)/$', views.support_issue, name="support_issue"),
     url(r'^logout/$', views.logout_view, name="logout_view"),
 ]
->>>>>>> upstream/master
